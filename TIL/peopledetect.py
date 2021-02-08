a4872fd921457b2bd32000ddf5d12c300431b7a3
FLY-CODE77 <lightyear94122@gmail.com>
2021-02-08 13:28:18 +0900

    Code: walking people detection

---
+++ TIL/peopledetect.py
@@ -0,0 +1,28 @@
+import sys
+import numpy as np
+import sys
+import cv2
+import random
+
+cap = cv2.VideoCapture('vtest.avi')
+
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+while True:
+    ret , frame = cap.read()
+
+    if not ret:
+        break
+    
+    detected, _ = hog.detectMultiScale(frame)
+
+    for (x, y, w, h) in detected:
+         c = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+         cv2.rectangle(frame, (x,y,w,h), c, 3)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(10) == 27:
+        break
+cv2.destroyAllWindows()
